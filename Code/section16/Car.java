a1b5b23388b75917ed1e7f9db9671dd8decc5311
tmax818 <tylermaxwell661@gmail.com>
2023-08-06 15:57:47 -0600

    updarte

---
+++ Code/section16/Car.java
@@ -0,0 +1,10 @@
+package Code.section16;
+
+public class Car {
+
+    String make;
+    double price;
+    int year;
+    String color;
+    
+}
---
+++ Code/section16/Main.java
@@ -0,0 +1,12 @@
+package Code.section16;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Car nissan = new Car();
+        System.out.println(nissan);
+        
+    }
+    
+}
